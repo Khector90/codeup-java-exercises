b54bf84feae6387e3af8d3c736c9c3587daef826
Kiley Hector <kiley.hector@gmail.com>
2022-11-08 12:27:16 -0500

    Fnished exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        String myString = 'c';
-        System.out.print(myString);
+        byte x = 12;
+
+
+        System.out.println(x);
 
     }
 }
