d644ea133db41bcdd257d330b975587d950d8971
Kiley Hector <kiley.hector@gmail.com>
2022-11-09 14:47:18 -0500

    Not done and can't focus

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -4,36 +4,32 @@ import static java.lang.Integer.parseInt;
 
 public class ConsoleExercises {
     public static void main(String[] args) {
-        double pi = 3.14159;
-        int pie = 3;
-        Scanner scanner = new Scanner(System.in);
+//        double pi = 3.14159;
+//        int pie = 3;
+//        Scanner scanner = new Scanner(System.in);
 
 //        System.out.printf("Please enter a number: ");
 //        int userInput = scanner.nextInt();
 //
 //        System.out.println("Is this your number " + userInput);
 
-        System.out.print("Enter a width ");
-        int userInput1 = parseInt(scanner.nextLine());
-//        String userInput2 = scanner.next();
-//        String userInput3 = scanner.next();
-        System.out.print("Enter length ");
-        int userInput2 = parseInt(scanner.nextLine());
+//        System.out.print("Enter a width ");
+//        int userInput1 = parseInt(scanner.nextLine());
+////        String userInput2 = scanner.next();
+////        String userInput3 = scanner.next();
+//        System.out.print("Enter length ");
+//        int userInput2 = parseInt(scanner.nextLine());
+//
+//        int totalWidth = userInput1 * userInput2;
+//
+//
+//        System.out.print(totalWidth);
+        int age =32;
+        Scanner myScanner = new Scanner(System.in);
+        int userAge = myScanner.nextInt();
+        System.out.println(userAge);
 
-        int totalWidth = userInput1 * userInput2;
-
-
-        System.out.print(totalWidth);
-
-
-
-
-
-
-
-
-
-
+        System.out.println(age == userAge);
 
     }
 }
