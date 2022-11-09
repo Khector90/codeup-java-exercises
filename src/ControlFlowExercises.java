e472401a1d32be28f140fc1b79312032b7311480
Kiley Hector <kiley.hector@gmail.com>
2022-11-09 15:31:10 -0500

    I still suck

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -6,6 +6,13 @@ public class ControlFlowExercises {
             }
         int  a = 0;
 
+        do {
+            System.out.println(a);
+            a += 2;
+
+
+        } while(a < 102);
+
 
     }
 
