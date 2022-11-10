ee855cfb4f08d6b97f7b978ae7fad72ebe00b078
Kiley Hector <kiley.hector@gmail.com>
2022-11-10 15:40:44 -0500

    Got some of it done

---
+++ src/StringExercise.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+
+
+
+public class StringExercise {
+    public static void main(String[] args){
+        String message = "We don't need no education";
+        System.out.println(message);
+        String message2 = "We don't need no though control";
+        System.out.println(message2);
+        String message3 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(message3);
+        String message4 = "In windows, the main drive is usually C:\\";
+        System.out.println(message4);
+        String message5 = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(message5);
+
+}
+}
