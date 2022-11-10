4453c1c592d5e0e21df47394aea2ba2885c0ea42
Kiley Hector <kiley.hector@gmail.com>
2022-11-10 17:18:18 -0500

    Not working jus tcoy pasta some stuff

--- src/Bob.java
+++ src/Bob.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
+
 public class Bob {
+    String hey(String remark) {
+        if (isYelling(remark) && isAQuestion(remark)) {
+            return "Calm down, I know what I'm doing!";
+        } else if (isYelling(remark)) {
+            return "Whoa, chill out!";
+        } else if (isAQuestion(remark)) {
+            return "Sure.";
+        } else if (isSilence(remark)) {
+            return "Fine. Be that way!";
+        } else {
+            return "Whatever.";
+        }
+
+        private boolean isAQuestion (String remark){
+            return remark.trim().endsWith("?");
+        }
+        private boolean isYelling (String remark){
+            return remark.equals(remark.toUpperCase()) && !remark.equals(remark.toLowerCase());
+        }
+        private boolean isSilence (String remark){
+            return remark.trim().isEmpty();
+        }
+    }
 }
