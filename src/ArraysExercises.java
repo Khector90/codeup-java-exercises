2e315e62299f1d35035c4ca47787a49e9bd3616e
Kiley Hector <kiley.hector@gmail.com>
2022-11-22 12:44:24 -0500

    Pluggin away still behind

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
-
+import java.util.Arrays;
+import java.util.Random;
 
 
 public class ArraysExercises {
+    public static void main(String[] args) {
+
+    }
+//    need to make a main method
+//    create an array that holds 3 people, iterate through the array and then get it to print out
 }
+
