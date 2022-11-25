562b9e510de9b98b83642e6be988b5098c34b0d3
Kiley Hector <kiley.hector@gmail.com>
2022-11-25 09:34:28 -0500

    Working on my stuff

---
+++ src/main.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+//public class pratice_stuff {
+//    public static void main(String[] args) {
+//        int[] arrayOne = {
+//                1789, 2035, 1899, 1456, 2013, 1458, 2458, 1254, 1472, 2365, 1456, 2165, 1457, 2456,};
+//
+//        String [] arrayTwo = {
+//                "java", "python", "PHP", "C#",
+//                "C Programming", "C++"
+//        };
+//        System.out.println("OG numbers array: " + Arrays.toString(arrayOne));
+//        Arrays.sort(arrayOne);
+//        System.out.println("Sorted Numbers array: " + Arrays.toString(arrayOne));
+//
+//        System.out.println("OG String array: " + Arrays.toString(arrayTwo));
+//        System.out.println("Sorted String array: " + Arrays.toString(arrayTwo));
+//    }
+    public class main{
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Input first number: ");
+            int num1 = input.nextInt();
+            System.out.println("Input the 2nd");
+            int num2 = input.nextInt();
+            int sum = num1 + num2;
+            System.out.println();
+            System.out.println("sum: " + sum);
+
+            System.out.println(50/3);
+
+        }
+    }
+
+
+
