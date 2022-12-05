661e036d3946851b95571802eb1907d06227212a
Kiley Hector <kiley.hector@gmail.com>
2022-12-04 20:53:09 -0500

    put in getter for movie app

--- src/Movies/MoviesApplication.java
+++ src/Movies/MoviesApplication.java
@@ -1,4 +1,24 @@
 package Movies;
 
 public class MoviesApplication {
+    private String getName;
+
+    private String category;
+
+    public movieGetSet(String getName, String category){
+        this.getName = getName;
+        this.category = category;
+    }
+       public String getGetName(){
+        return getName;
+       }
+       public String getCategory(){
+        return category;
+       }
+
+
 }
+
+
+
+
