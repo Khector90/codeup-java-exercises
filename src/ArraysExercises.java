0cda2244a9cc109b59caba1b9cdca0b25a6b4d75
Kiley Hector <kiley.hector@gmail.com>
2022-12-04 20:41:10 -0500

    finished part two '

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -5,16 +5,24 @@ import java.util.Arrays;
 
 
 public class ArraysExercises {
+
     //    need to make a main method
     public static <Dogs> void main(String[] args) {
         //    create an array that holds 3 people, iterate through the array and then get it to print out
    String[] Persons = {"Ed", "Edd", "Eddy"};
         System.out.println(Arrays.toString(Persons));
+        String [] addPerson = Arrays.copyOf(Persons, Persons.length +1);
+        addPerson[Persons.length] = "bert";
 
-        Dogs[] dog= new Dogs[3];
-        dog[0] = new Dogs("Hobie");
-        dog[1] = new Dogs("Shimmy");
-        dog[2] = new Dogs("Jango");
+        for(String people: addPerson){
+            System.out.println(people);
+            System.out.println(Arrays.toString(addPerson));
+        }
+
+//        Dogs[] dog= new Dogs[3];
+//        dog[0] = new Dogs("Hobie");
+//        dog[1] = new Dogs("Shimmy");
+//        dog[2] = new Dogs("Jango");
 
 
 
@@ -24,10 +32,10 @@ public class ArraysExercises {
    }
 //   creating the add person method
     int five = 5;
-public static void addPerson(Person[] array, Person newPerson){
-
-
-}
+//    public static void addPerson(Person[] array, Person newPerson){
+//        String addPerson = Arrays.copyof(Persons, Persons.length +1);
+//
+//    }
 
     }
 
